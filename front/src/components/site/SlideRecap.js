785c66d8a2cb1681dc66926f5f40cbbba7702c54
CedricT33 <cedric.tressous@gmail.com>
2023-06-29 17:20:47 +0200

    modif adresse mail

--- front/src/components/site/SlideRecap.js
+++ front/src/components/site/SlideRecap.js
@@ -14,14 +14,14 @@ export default function SlideRecap() {
         </div>
       </div>
       <div className="reseaux-sociaux">
-        <a href='mailto:maglv@free.fr' className="mail"><span className='offscreen'>Mail de MLV</span></a>
+        <a href='mailto:maglv@orange.fr' className="mail"><span className='offscreen'>Mail de MLV</span></a>
         <a href='https://www.facebook.com/mlvaisselle/' className="facebook"><span className='offscreen'>Facebook de MLV</span></a>
         <a href='https://www.mariages.net/decoration-mariage/magali-location-vaisselle--e11271' className="coeur"><span className='offscreen'>Mariage point net</span></a>
       </div>
       <div className="tel">
         <div><span>Tel</span> : 03.44.42.35.69</div>
         <div><span>Portable</span> : 06.61.40.30.75</div>
-        <div><span>E mail</span> : maglv@free.fr</div>
+        <div><span>E mail</span> : maglv@orange.fr</div>
       </div>
     </div>
   );
